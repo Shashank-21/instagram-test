a05ac5eb6abf2f3fe34c220e1288bb3a0c976e46
Shashank B R <shashankbr21494@gmail.com>
2023-01-21 20:32:01 +0530

    working!

--- src/App.js
+++ src/App.js
@@ -1,4 +1,32 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
-  return <div>App</div>;
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    async function getPosts() {
+      const userID = "6406781739351055";
+      const userName = "shashank_b_r_21";
+      const accessToken =
+        "IGQVJYbDZADMldDWTlTTmhBWmRtenpLNlRFdXRlZAU5sY3cyRFlTZA3JPaWRGZAWg4TnNIZA0FQbDV4allSeWswZAnBIYnJqRVp4X0tIVVE0SkE4TGREQjFwTHZAXUVdsaFRDazgxTVJSN1Y1Q0RxbkI1SUxVTk5pZAU93YlYxczc0";
+      const posts = await axios.get(
+        `https://graph.instagram.com/me/media?fields=id,media_type,media_url,username,timestamp&access_token=${accessToken}`
+      );
+      setPosts(posts.data.data);
+    }
+    getPosts();
+  }, []);
+
+  return (
+    <div>
+      {posts.map((post, index) => {
+        if (post.media_type === "VIDEO") {
+          return <iframe src={post.media_url} title={post.id} key={index} />;
+        } else {
+          return <img src={post.media_url} alt={post.id} key={index} />;
+        }
+      })}
+    </div>
+  );
 }
 export default App;
